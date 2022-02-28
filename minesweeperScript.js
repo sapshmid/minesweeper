1f0fc6e0f08da734fb1c6fe3b40241be13f2beed
Sapir <sapirco5400@gmail.com>
2022-02-28 11:52:46 +0200

    Fixed array and placed numbers around the bombs

--- minesweeperScript.js
+++ minesweeperScript.js
@@ -1,7 +1,8 @@
 const heightElement = document.getElementById('field-height');
 const widthElement = document.getElementById('field-width');
+const bombsNumElement = document.getElementById('total-bombs-num');
 
-function printSize(event) {
+/*function printSize(event) {
     console.log(heightElement.value);
     console.log(widthElement.value);
     console.log(event.target);
@@ -14,72 +15,54 @@ function printSize(event) {
 document.getElementById('start-button').addEventListener('click', printSize);
 [...document.getElementsByClassName('field-cell-box')].forEach(cell => {
     cell.addEventListener('click', printSize);
-});
-// totalCells is the size of the field - multipiction of the height and width
-const totalCells = heightElement.value * widthElement.value;
-// console.log(totalCells);    
-// Creat the field as an array and make all it's values 0
-const fieldArray = [];
-for (let i = 0; i < totalCells; i++) {
-    fieldArray.push(0);
+});*/
+function startGame() {
+    // totalCells is the size of the field - multipiction of the height and width
+    const heightStart = Number(heightElement.value);
+    const widthStart = Number(widthElement.value);
+    // Get the number of bombs from the input
+    const bombsStart = Number(bombsNumElement.value);
+    const field = createField(heightStart, widthStart, bombsStart);
+    console.log(field);
 }
-console.log(fieldArray);
 
-
-// Get the number of bombs from the input
-const bombsNum = document.getElementById('total-bombs-num');
-// Counter for bombs placed 
-let bombsPlaced = 0;
-// Loop placing the bombs on the field according to input amount of bombs 
-while (bombsPlaced < bombsNum.value) {
-    // Get random index in the array and make sure not to use the same index more then once
-    let randomIndex = Math.floor(Math.random() * fieldArray.length);
-    while (fieldArray[randomIndex] === '*') {
-        randomIndex = Math.floor(Math.random() * fieldArray.length);
+function createField(heightValue, widthValue, bombsValue) {
+    const totalCells = heightValue * widthValue;
+    // Creat the field as an array and make all it's values 0
+    const fieldArray = [];
+    for (let i = 0; i < totalCells; i++) {
+        fieldArray.push(0);
     }
-    // Place * in the index chosen as a simble for bomb
-    fieldArray[randomIndex] = '*';
-    console.log(randomIndex);
-    // Increas counter
-    bombsPlaced++;
-}
-// Print the array/field
-console.log(fieldArray);
-
-
-
-
-
-
-
-
-/*const height = document.getElementById('field-height').value;
-const width = document.getElementById('field-width').value;
-console.log(height, width);
-
-function setInputValue(fieldheight, height) {
-    document.getElementById('field-height').setAttribute('value', height);
-}
-
-function setInputValue(fieldwidth, width) {
-    document.getElementById('field-width').setAttribute('value', width);
-}
-
-function startGame(height, width) {
-    const table = document.getElementById('field-table');
-    for (let i = 0; i < width; i++) {
-        const row = table.insertRow(i);
+    // Counter for bombs placed 
+    let bombsPlaced = 0;
+    // Loop placing the bombs on the field according to input amount of bombs 
+    while (bombsPlaced < bombsValue) {
+        // Get random index in the array and make sure not to use the same index more then once
+        let randomIndex = Math.floor(Math.random() * fieldArray.length);
+        while (fieldArray[randomIndex] === '*') {
+            randomIndex = Math.floor(Math.random() * fieldArray.length);
+        }
+        // Place * in the index chosen as a simble for bomb
+        fieldArray[randomIndex] = '*';
+        // Increas counter
+        bombsPlaced++;
     }
-    for (let i = 0; i < height; i++) {
-        const cell = row.insertCell(i);
-        cell.innerHTML = '<div class="field-cell-box"> </div>';
-    }
+    for (let i = 0; i < totalCells; i++) {
+        if (fieldArray[i] === '*') continue;
+        const isTopLine = i < widthValue;
+        const isBottomLine = i > fieldArray.length - widthValue;
+        const isRightLine = i % widthValue === widthValue - 1;
+        const isLeftLine = i % widthValue === 0;
 
+        if (fieldArray[i - 1] === '*' && !isLeftLine) fieldArray[i] += 1;
+        if (fieldArray[i + 1] === '*' && !isRightLine) fieldArray[i] += 1;
+        if (fieldArray[i - widthValue] === '*' && !isTopLine) fieldArray[i] += 1;
+        if (fieldArray[i - widthValue + 1] === '*' && !isTopLine && !isRightLine) fieldArray[i] += 1;
+        if (fieldArray[i - widthValue - 1] === '*' && !isTopLine && !isLeftLine) fieldArray[i] += 1;
+        if (fieldArray[i + widthValue] === '*' && !isBottomLine) fieldArray[i] += 1;
+        if (fieldArray[i + widthValue + 1] === '*' && !isBottomLine && !isRightLine) fieldArray[i] += 1;
+        if (fieldArray[i + widthValue - 1] === '*' && !isBottomLine && !isLeftLine) fieldArray[i] += 1;
+    }
+    return fieldArray;
 }
 document.getElementById('start-button').addEventListener('click', startGame);
-
-function cellNumbers() {
-    document.getElementsByClassName('field-data').innerHTML = '<div class="field-cell-box"> 1 </div>';
-}
-
-document.getElementsByClassName('field-data').addEventListener('click', cellNumbers);*/
